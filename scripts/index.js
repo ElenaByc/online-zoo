a9bd56da7d60b300579e50ca4ae29f86f247cdff
Elena Bychenkova <elena.bychenkova@gmail.com>
2022-10-31 17:00:55 -0700

    feat: move the project to another repo

---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+// alert("Уважаемые проверяющие!\nПожалуйста-пожалуйста, можете подождать с проверкой пару дней?\nМожет хоть что-то успею доделать.\nК сожалению, совсем не было возможности работать над проектом.\nСпасибо!");
+
+alert(`Hello!
+Could you please wait for a couple of days so that I can finish the work? 
+Sorry. I was not able to do it on time. 
+Thank you so much for understanding!`);
